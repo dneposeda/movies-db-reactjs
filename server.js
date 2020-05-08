9ff035e58a49c603e59bae3dc91069bd36a9600f
Denis Tivikov <denis_tivikov@epam.com>
2020-05-08 20:32:35 +0400

    [task-01] add 4 react components

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 const server = express();
 const PORT = 3000;
+const PUBLIC_PATH = __dirname + '/dist';
 
-server.get('/', (req, res) => res.send('Hello World!'))
+server.use(express.static(PUBLIC_PATH));
+server.get('/', (req, res) => res.sendFile(path.resolve(PUBLIC_PATH, 'index.html')))
 
 server.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
